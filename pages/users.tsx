e9c05dcff8c117cf6b216d01a2a1656c4844399f
jgram <jprgr16@gmail.com>
2023-04-25 23:32:49 +0900

    first commit

---
+++ pages/users.tsx
@@ -0,0 +1,12 @@
+import BlankLayout from "@/components/blank-layout";
+import {NextPageWithLayout} from "@/pages/_app";
+import {ReactElement} from "react";
+
+const Page: NextPageWithLayout = () => {
+  return (
+    <main>
+    </main>
+  )
+}
+
+export default Page
